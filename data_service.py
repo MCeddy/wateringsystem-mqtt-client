9cd175abbd45538b815c80ac7c717fc00318750a
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2017-07-17 10:44:43 +0200

    add exception handling for MySQL connection error

--- data_service.py
+++ data_service.py
@@ -1,13 +1,17 @@
 import MySQLdb
+import _mysql_exceptions
 
 
 class DataService:
     def __init__(self, mysql_config):
         self.config = mysql_config
-        self.db = MySQLdb.connect(host=mysql_config['host'],
-                                  db=mysql_config['database'],
-                                  user=mysql_config['user'],
-                                  passwd=mysql_config['password'])
+        try:
+            self.db = MySQLdb.connect(host=mysql_config['host'],
+                                      db=mysql_config['database'],
+                                      user=mysql_config['user'],
+                                      passwd=mysql_config['password'])
+        except _mysql_exceptions.OperationalError:
+            print('couldn\'t connect to MySQL database')
 
     def save_temperature(self, value):
         pass
