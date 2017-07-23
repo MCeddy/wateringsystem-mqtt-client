2603d60bf12bbf9183e0f9036bab69b2afd1c23b
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2017-07-23 14:16:46 +0200

    optimize code

--- main.py
+++ main.py
@@ -6,12 +6,30 @@ from data_service import DataService
 from config_service import ConfigService
 
 
+def load_args():
+    # setup commandline argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env')
+
+    return parser.parse_args()
+
+
+def create_mqtt_client(config):
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.username_pw_set(config['user'], config['password'])
+    client.connect(config['host'], config['port'])
+
+    return client
+
+
 def on_connect(client, userdata, flags_dict, rc):
     if rc != 0:
-        print('connection error: ' + str(rc))
+        print('MQTT connection error: ' + str(rc))
         return
 
-    print('connected')
+    print('MQTT connected')
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -29,21 +47,13 @@ def on_message(client, userdata, msg):
 
     data_service.save_sensor_values(temperature, humidity, soil_moisture)
 
-# setup commandline argument parser
-parser = argparse.ArgumentParser()
-parser.add_argument('--env')
-args = parser.parse_args()
+args = load_args()
 
 config_service = ConfigService(args.env)
 mqtt_config = config_service.get_section('mqtt')
 mysql_config = config_service.get_section('mysql')
+
 data_service = DataService(mysql_config)
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.username_pw_set(mqtt_config['user'], mqtt_config['password'])
-
-client.connect(mqtt_config['host'], mqtt_config['port'])
-
-client.loop_forever()
+mqtt_client = create_mqtt_client(mqtt_config)
+mqtt_client.loop_forever()
