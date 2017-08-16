1f3a17a993bda19a104fc0f039e147287293fd8d
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2017-08-16 17:47:10 +0200

    optimize code

--- services/watering_service.py
+++ services/watering_service.py
@@ -1,11 +1,14 @@
 class WateringService:
     def __init__(self, watering_config):
-        self.__config = watering_config
+        config = watering_config
+
+        self.__min_soil_moisture = int(config['minSoilMoisture'])
+        self.__watering_milliseconds = int(config['wateringMilliseconds'])
 
     def calculate_milliseconds(self, soil_moisture):
-        if soil_moisture > int(self.__config['minSoilMoisture']):
+        if soil_moisture > self.__min_soil_moisture:
             # no watering
             return 0
 
         # watering
-        return int(self.__config['wateringMilliseconds'])
+        return self.__watering_milliseconds
