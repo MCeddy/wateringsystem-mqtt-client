e9a8f10c60ca4a1ae9e55d96cddfe6cc43d9ac61
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2017-08-16 15:14:03 +0200

    fix load config file by environment console argument

--- config_service.py
+++ config_service.py
@@ -9,22 +9,25 @@ class ConfigService:
         self.__load_from_file(env)
 
     def __load_from_file(self, env):
-        file_name = self.__get_file_name(env)
-        config_path = os.path.join(os.getcwd(), 'config', file_name)
+        config_path = self.__get_config_path(env)
 
         with open(config_path, 'r') as file:
             self.__cfg = yaml.safe_load(file)
 
-    def __get_file_name(self, env):
+    def __get_config_path(self, env):
+        config_folder = os.path.join(os.getcwd(), 'config')
+
         if env is not None:
             env_file_name = 'config_{}.yml'.format(env)
+            env_config_path = os.path.join(config_folder, env_file_name)
             
-            if os.path.isfile(env_file_name):  # exists
+            if os.path.isfile(env_config_path):  # exists
                 self.__logger.debug('load environment configuration for "{}"'.format(env))
-                return env_file_name
+                return env_config_path
 
         self.__logger.debug('load default configuration')
-        return 'config.yml'
+        config_path = os.path.join(config_folder, 'config.yml')
+        return config_path
 
     def get_section(self, section_name):
         if section_name not in self.__cfg:
